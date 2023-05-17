1a29a4980edd8a1eabb6e46b07935e58aa803a42
zinakalu <zina.kalu@gmail.com>
2023-05-17 07:29:03 -0400

    edited code

--- SongV2.java
+++ SongV2.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
 
-class SongV2{
+class SongV2 implements Comparable<SongV2>{
     private String title;
     private String artist;
     private int bpm; //3 instance variables for the 3 song attributes in the file
@@ -10,6 +10,10 @@ class SongV2{
         this.title = title;
         this.artist = artist;
         this.bpm = bpm;
+    }
+
+    public int compareTo(SongV2 other){
+        return this.title.compareTo(other.title);
     }
 
     public String getTitle(){
@@ -31,8 +35,10 @@ class SongV2{
 
 class MockSong{
     public static List<String> getSongStrings(){
-         List<String> songStrings = newArrayList<>();
-    return songStrings;
+        List<String> songStrings = new ArrayList<>();
+        return songStrings;
+
+
     }
 
     public static List<SongV2> getSongsV2(){
@@ -46,5 +52,24 @@ class MockSong{
         songs.add(new SongV2 ("Cassidy", "grateful dead", 158));
         songs.add(new SongV2 ("50 ways", "simon", 102));
         return songs;
+    }
+
+    public static void main(String[] args){
+        //Test getSongStrings()
+        List<String> songStrings = MockSong.getSongString();
+        System.out.println("Song Strings: " + songStrings);
+
+        //Test getSongsV2()
+        List<SongV2> songs = MockSong.getSongsV2();
+        System.out.println("Songs: " + songs);
+
+        //Iterate over the SongV2 objects and print their details
+        for (SongV2 song : songs){
+            System.out.println("Title: " + song.getTitle());
+            System.out.println("Artist: " + song.getArtist());
+            System.out.println("BPM: " + song.getBpm());
+            System.out.println();
+        }
+
     }
 }
